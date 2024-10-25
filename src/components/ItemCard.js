49c3aeecbc1113bdbd3390d89b1b315c99153db3
Valentina <milicevic.vanja.91@gmail.com>
2024-10-25 13:24:05 +0200

    Added Components and styles to src

---
+++ src/components/ItemCard.js
@@ -0,0 +1,13 @@
+import styles from '../styles/itemCard.module.scss'
+
+const itemCard = () => {
+    return (
+        <div className={styles['card-container']}>
+            <h1>
+                This is item card
+            </h1>
+        </div>
+    )
+}
+
+export default itemCard
